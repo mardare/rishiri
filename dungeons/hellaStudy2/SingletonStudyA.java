1ca37aed49be4b240f75f8b2b6b955afc5708927
mrd@w <n/a>
2015-02-26 15:16:19 +0200

    dungeons re-org; tmp

---
+++ dungeons/hellaStudy2/SingletonStudyA.java
@@ -0,0 +1,36 @@
+package hellaStudy2;
+
+//singleton defined in singletonStudyB
+
+public class SingletonStudyA {
+
+	public static void main(String[] args) {
+
+//		try {
+//		
+//			//// testing "new" disabled - throws an uncatchable java.lang.Error
+//			//singletonStudyB s= new singletonStudyB();
+//		
+//		} catch (Throwable t) {
+//			System.out.println("...trying to catch a java.lang.Error");
+//		} finally {
+//			System.out.println("...finally");
+//		}
+//		
+
+
+		SingletonStudyB s = SingletonStudyB.getInstance();
+		
+
+		
+		// SingletonStudyB s2= s.clone(); 	
+		// error The method clone() from the type Object is not visible
+		//      protected native Object clone() throws CloneNotSupportedException;
+		
+		SingletonStudyClonable s2=SingletonStudyClonable.getInstance();
+		SingletonStudyClonable s2bis=(SingletonStudyClonable) s2.clone();
+
+		System.out.println("end-of-program");
+	}
+
+}
