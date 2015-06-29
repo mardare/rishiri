325c6f00fd863c7112ec2b5c56df0ce43e4ffa30
mrd@m <n/a>
2015-06-29 21:11:42 +0300

    coffee; chapters-js

--- dungeons/XVIIIth/bash/coffee.sh
+++ dungeons/XVIIIth/bash/coffee.sh
@@ -135,6 +135,7 @@ dq=(
   	"The next best thing to having good ideas is recognizing good ideas -- Eric S. Raymond"
 	"habeas corpus -- 'you have the body' in Latin, technically only a procedural remedy; it is addressed to the custodian, to present proof of authority, allowing the court to determine whether the custodian has lawful authority to detain the prisoner;"
 	"Nothing in life is to be feared, it is only to be understood -- Marie Curie"
+	"subvert the dominant paradigm -- yanis varoufakis"
 
  
 
