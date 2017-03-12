57eb9c16d704548c4b5a9428de522f7d17a44d25
mrd <mardare@maison>
2017-03-12 20:54:07 +0200

    groovy fix

--- dungeons/groovy/groovy-sqlite-study.groovy
+++ dungeons/groovy/groovy-sqlite-study.groovy
@@ -2,6 +2,7 @@
 /*${user.home}/.groovy/grapes/org.xerial/sqlite-jdbc/jars
 * alt sqlite-jdbc-3.15.1.jar@https://bitbucket.org/xerial/sqlite-jdbc/downloads
 */
+
 @Grapes([
  @Grab(group='org.xerial',module='sqlite-jdbc',version='3.15.1'),
  @GrabConfig(systemClassLoader=true)
@@ -10,12 +11,10 @@
 import java.sql.*;
 import groovy.sql.Sql;
 
-import org.sqlite.SQLite;
+import org.sqlite.*;
 
 def sql = Sql.newInstance("jdbc:sqlite:c:\\temp\\rishiri\\dungeons\\groovy\\groovy-sqlite-study.db", "org.sqlite.JDBC")
 
 sql.eachRow("select * from hella") {  
  println("item=${it.item}, prince= ${it.price}") 
 }
-
-
